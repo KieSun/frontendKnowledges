075cbef0b1323d8a419b38d5f8a8b5288877c959
yck <>
2018-11-08 17:02:55 +0800

    simple promise

---
+++ 04-手写 Promise/simplePromise.js
@@ -0,0 +1,65 @@
+const PENDING = 'pending'
+const RESOLVED = 'resolved'
+const REJECTED = 'rejected'
+
+function MyPromise(fn) {
+  const that = this
+  that.state = PENDING
+  that.value = null
+  that.resolvedCallbacks = []
+  that.rejectedCallbacks = []
+  try {
+    fn(resolve, reject)
+  } catch (e) {
+    that.reject(e)
+  }
+
+  function resolve(value) {
+    if (that.state === PENDING) {
+      that.state = RESOLVED
+      that.value = value
+      that.resolvedCallbacks.map(cb => cb(that.value))
+    }
+  }
+
+  function reject(value) {
+    if (that.state === PENDING) {
+      that.state = REJECTED
+      that.value = value
+      that.rejectedCallbacks.map(cb => cb(that.value))
+    }
+  }
+}
+
+MyPromise.prototype.then = function(onFulfilled, onRejected) {
+  const that = this
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+  onRejected =
+    typeof onRejected === 'function'
+      ? onRejected
+      : r => {
+          throw r
+        }
+  if (that.state === PENDING) {
+    that.resolvedCallbacks.push(onFulfilled)
+    that.rejectedCallbacks.push(onRejected)
+  }
+  if (that.state === RESOLVED) {
+    onFulfilled(that.value)
+  }
+  if (that.state === REJECTED) {
+    onRejected(that.value)
+  }
+}
+
+let p = new MyPromise((resolve, reject) => {
+  // setTimeout(() => {
+  //   resolve(1)
+  // }, 0)
+  resolve(1)
+  reject(2)
+})
+
+p.then(value => {
+  console.log(value)
+})
